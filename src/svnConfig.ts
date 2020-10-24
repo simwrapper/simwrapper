0a5edcde8ec4324b84a460cd0ece3e3a224fefe3
Billy Charlton <billy@okbecause.com>
2020-10-25 01:58:22 +0200

    examples

--- src/svnConfig.ts
+++ src/svnConfig.ts
@@ -1,6 +1,15 @@
 const config: any = {
   projects: [
     {
+      name: 'Sample Visualizations',
+      url: 'example',
+      description: 'A few visualizations of public datasets.',
+      svn:
+        'https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/viz-examples/examples',
+      need_password: false,
+      thumbnail: '/thumb-examples.jpg',
+    },
+    {
       name: 'Gladbeck',
       url: 'gladbeck',
       description: 'AVÖV Projekt: Gladbeck NRW',
