f9d7721ec5ebcf9429b74d4d9ba8d91146597db9
Billy Charlton <billy@okbecause.com>
2021-06-16 18:08:17 +0200

    re-add BlankComponent so smoke test works

---
+++ src/components/BlankComponent.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+#vue-component
+  h1 Blank
+
+</template>
+
+<script lang="ts">
+import { Vue, Component, Watch, Prop } from 'vue-property-decorator'
+
+@Component({ components: {}, props: {} })
+export default class VueComponent extends Vue {
+  // @Prop({ required: true })
+  // private viz!: Viz
+}
+</script>
+
+<style scoped lang="scss">
+@import '@/styles.scss';
+
+@media only screen and (max-width: 640px) {
+}
+</style>
