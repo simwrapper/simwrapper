09065bfd7edb62d60dd3a09d7ee3809a1c0c41a2
Billy Charlton <billy@okbecause.com>
2021-07-12 11:28:13 +0200

    add 'npm run deploy' to manually push build to github pages

---
+++ scripts/deploy-ghpages.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# DEPLOY: Build site into /dist folder and PUSH to gh-pages branch ON GITHUB
+set -euo pipefail
+
+echo --- BUILDING ---
+npm run build
+
+echo --- CREATING GIT COMMIT FOR GH-PAGES ---
+cd dist
+git init .
+git add . && git commit -m "gh-pages"
+
+echo --- PUSHING TO GITHUB ---
+git remote add origin git@github.com:aftersim/aftersim.github.io.git
+git push --force origin master:master
+
