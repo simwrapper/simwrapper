8ca422f4e262027bae5a87d8c3386fe6fb0bef33
Billy Charlton <billy@okbecause.com>
2021-12-01 15:25:27 +0100

    feat: Support multiple local servers on ports 8000-8500
    The new python "simwrapper" library can open multiple local servers.
    Run "pip install simwrapper" and then you can:
    
    - simwrapper serve, to do the usual one-server
    - simwrapper open vsp|asim, to open a browser tab for the current folder

--- src/fileSystemConfig.ts
+++ src/fileSystemConfig.ts
@@ -54,4 +54,16 @@ const fileSystems: FileSystemConfig[] = [
   // },
 ]
 
+for (let port = 8000; port < 8500; port++) {
+  fileSystems.push({
+    name: 'Localhost ' + port,
+    slug: `${port}`,
+    description: 'Localhost ' + port,
+    description_de: 'Localhost ' + port,
+    baseURL: 'http://localhost:' + port,
+    thumbnail: '/simwrapper/images/thumb-localfiles.jpg',
+    hidden: true,
+  })
+}
+
 export default fileSystems
