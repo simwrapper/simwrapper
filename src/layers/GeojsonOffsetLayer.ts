37c9cbf2f412fec39221b9b57a34d0dc2a369435
Billy Charlton <charlton@vsp.tu-berlin.de>
2022-05-16 14:53:17 +0200

    path layers: offset works

---
+++ src/layers/GeojsonOffsetLayer.ts
@@ -0,0 +1,138 @@
+import { GeoJsonLayer } from '@deck.gl/layers'
+import PathOffsetLayer from '@/layers/PathOffsetLayer'
+import { forwardProps } from '@deck.gl/layers/src/geojson-layer/sub-layer-map'
+
+const OFFSET_TYPE = {
+  NONE: 0,
+  LEFT: 1,
+  RIGHT: 2,
+}
+
+export const LINE_LAYER = {
+  type: PathOffsetLayer,
+  props: {
+    lineWidthUnits: 'widthUnits',
+    lineWidthScale: 'widthScale',
+    lineWidthMinPixels: 'widthMinPixels',
+    lineWidthMaxPixels: 'widthMaxPixels',
+    lineJointRounded: 'jointRounded',
+    lineCapRounded: 'capRounded',
+    lineMiterLimit: 'miterLimit',
+    lineBillboard: 'billboard',
+    getLineColor: 'getColor',
+    getLineWidth: 'getWidth',
+  },
+}
+
+export default class GeojsonOffsetLayer extends GeoJsonLayer {
+  constructor(props: any) {
+    super(props)
+  }
+
+  // this is copied directly from @deck.gl/layers/geojson-layer
+  _renderLineLayers() {
+    const { extruded, stroked } = this.props
+    const { layerProps } = this.state
+    const polygonStrokeLayerId = 'polygons-stroke'
+    const lineStringsLayerId = 'linestrings'
+
+    const PolygonStrokeLayer =
+      !extruded &&
+      stroked &&
+      this.shouldRenderSubLayer(polygonStrokeLayerId, layerProps.polygonsOutline.data) &&
+      this.getSubLayerClass(polygonStrokeLayerId, LINE_LAYER.type)
+    const LineStringsLayer =
+      this.shouldRenderSubLayer(lineStringsLayerId, layerProps.lines.data) &&
+      this.getSubLayerClass(lineStringsLayerId, LINE_LAYER.type)
+
+    if (PolygonStrokeLayer || LineStringsLayer) {
+      const forwardedProps = forwardProps(this, LINE_LAYER.props)
+
+      return [
+        PolygonStrokeLayer &&
+          new PolygonStrokeLayer(
+            forwardedProps,
+            this.getSubLayerProps({
+              id: polygonStrokeLayerId,
+              updateTriggers: forwardedProps.updateTriggers,
+            }),
+            layerProps.polygonsOutline
+          ),
+
+        LineStringsLayer &&
+          new LineStringsLayer(
+            forwardedProps,
+            this.getSubLayerProps({
+              id: lineStringsLayerId,
+              updateTriggers: forwardedProps.updateTriggers,
+            }),
+            layerProps.lines
+          ),
+      ]
+    }
+    return null
+  }
+
+  getShaders() {
+    return {
+      ...super.getShaders(),
+      inject: {
+        'vs:#decl': `
+            attribute float instanceOffset;
+            varying float offset;
+            `,
+        'vs:#main-start': `
+            offset = instanceOffset;
+            `,
+        'fs:#decl': `
+            varying float offset;
+            `,
+        'fs:#main-start': `
+            if (offset == 1.0 && vPathPosition.x < 0.0) {
+                discard;
+            }
+            if (offset == 2.0 && vPathPosition.x > 0.0) {
+                discard;
+            }
+            if (offset == 0.0 && abs(vPathPosition.x) > 0.5) {
+                discard;
+            }
+        `,
+      },
+    }
+  }
+}
+
+GeojsonOffsetLayer.layerName = 'GeojsonOffsetLayer'
+GeojsonOffsetLayer.defaultProps = {
+  getOffset: { type: 'accessor', value: OFFSET_TYPE.RIGHT },
+}
+
+// /** DeckGL **/
+// new deck.DeckGL({
+//   container: 'container',
+//   mapboxApiAccessToken: '',
+//   longitude: -122.408,
+//   latitude: 37.785,
+//   zoom: 16,
+//   pitch: 0,
+//   layers: [
+//     new deck.PathLayer({
+//       data:
+//         'https://raw.githubusercontent.com/uber-common/deck.gl-data/master/website/bart-lines.json',
+//       getPath: d => d.path,
+//       getColor: [0, 0, 0],
+//       getWidth: 50,
+//     }),
+//     new MyPathLayer({
+//       data:
+//         'https://raw.githubusercontent.com/uber-common/deck.gl-data/master/website/bart-lines.json',
+//       getPath: d => d.path,
+//       getColor: [255, 0, 0],
+//       getOffset: (d, { index }) => index % 3,
+//       getWidth: 50,
+//       pickable: true,
+//       autoHighlight: true,
+//     }),
+//   ],
+// })
