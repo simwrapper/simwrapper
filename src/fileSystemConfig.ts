6a2efb25fb270dfef74947fc08455d9557647c72
Billy Charlton <charlton@vsp.tu-berlin.de>
2022-05-27 10:08:11 +0200

    fix: switch "simwrapper here" ports from 9039 -> 8050
    SANDAG firewall is blocking ports 9xxx, so let's move.

--- src/fileSystemConfig.ts
+++ src/fileSystemConfig.ts
@@ -34,7 +34,7 @@ const fileSystems: FileSystemConfig[] = [
     name: webLiveHostname + ' live folders',
     slug: 'live',
     description: 'Files served using "simwrapper here"',
-    baseURL: websiteLiveHost + ':9039/_f_', // e.g. 'http://localhost:9039/_f_',
+    baseURL: websiteLiveHost + ':8050/_f_', // e.g. 'http://localhost:8050/_f_',
     hidden: true,
   },
   {
@@ -86,7 +86,7 @@ const fileSystems: FileSystemConfig[] = [
   },
 ]
 
-for (let port = 8000; port < 8500; port++) {
+for (let port = 8000; port < 8049; port++) {
   fileSystems.push({
     name: 'Localhost ' + port,
     slug: `${port}`,
@@ -97,13 +97,13 @@ for (let port = 8000; port < 8500; port++) {
   })
 }
 
-for (let port = 9039; port < 9100; port++) {
+for (let port = 8050; port < 8099; port++) {
   fileSystems.push({
     name: webLiveHostname + port,
     slug: `${port}`,
     description: webLiveHostname + port,
     description_de: webLiveHostname + port,
-    baseURL: websiteLiveHost + `:${port}/_f_`, // e.g. 'http://localhost:9039/_f_',
+    baseURL: websiteLiveHost + `:${port}/_f_`, // e.g. 'http://localhost:8050/_f_',
     hidden: true,
   })
 }
