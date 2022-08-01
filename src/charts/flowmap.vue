6caa39ef6269b0754f5af0ff25931fa77db53d1d
j-bnsch <78746351+j-bnsch@users.noreply.github.com>
2022-08-01 11:24:49 +0200

    flowmap works as standalone and in a dashboard, no center/zoom & a bunch of errors

--- src/charts/flowmap.vue
+++ src/charts/flowmap.vue
@@ -1,158 +1,36 @@
 <template lang="pug">
-.map-layout
-  flow-map-layer.map-layer(v-if="centroids.length"
-    :viewId="viewId"
-    :props="mapProps"
-  )
+flow-map.deck-map(
+  :fsConfig="fileSystemConfig"
+  :root="fileSystemConfig.slug"
+  :subfolder="subfolder"
+  :configFromDashboard="config"
+  :thumbnail="false"
+  :datamanager="datamanager"
+  :yamlConfig="'config'"
+  @isLoaded="isLoaded"
+)
+
 
 </template>
 
 <script lang="ts">
 import { Vue, Component, Watch, Prop } from 'vue-property-decorator'
-import * as turf from '@turf/turf'
 
-import { FileSystemConfig, REACT_VIEW_HANDLES } from '@/Globals'
-import FlowMapLayer from '@/layers/FlowMapLayer'
-import HTTPFileSystem from '@/js/HTTPFileSystem'
+import { FileSystemConfig } from '@/Globals'
+import FlowMap from '@/plugins/flowmap/Flowmap.vue'
 import DashboardDataManager from '@/js/DashboardDataManager'
-// import globalStore from '@/store'
 
-import { VuePlugin } from 'vuera'
-Vue.use(VuePlugin)
-
-@Component({ components: { FlowMapLayer } as any })
+@Component({ components: { FlowMap } })
 export default class VueComponent extends Vue {
   @Prop({ required: true }) fileSystemConfig!: FileSystemConfig
   @Prop({ required: true }) subfolder!: string
   @Prop({ required: true }) files!: string[]
   @Prop({ required: true }) config!: any
-  @Prop({ required: true }) datamanager!: DashboardDataManager
+  @Prop({ required: false }) datamanager!: DashboardDataManager
+  @Prop({ required: false }) yamlConfig!: string
 
-  private fileApi!: HTTPFileSystem
-  private boundaries: any[] = []
-
-  private centroids: { id: any; name?: any; lat: number; lon: number }[] = []
-  private flows: any[] = []
-
-  private viewId = Math.random()
-
-  private get mapProps() {
-    return {
-      locations: this.centroids,
-      flows: this.flows,
-      dark: this.$store.state.isDarkMode,
-      elapsed: this.elapsed,
-    }
-  }
-
-  @Watch('$store.state.viewState') viewMoved() {
-    if (!REACT_VIEW_HANDLES[this.viewId]) return
-    REACT_VIEW_HANDLES[this.viewId]()
-  }
-
-  private async mounted() {
-    this.fileApi = new HTTPFileSystem(this.fileSystemConfig)
-
-    // load the boundaries and the dataset, use promises so we can clear
-    // the spinner when things are finished.  MUST be in this order
-    // or the flowmap gets sad if dataset loads faster than boundaries do.
-    await this.loadBoundaries()
-    await this.loadDataset()
-
-    this.updateChart()
-
+  private isLoaded() {
     this.$emit('isLoaded')
-
-    this.animate()
-  }
-
-  private beforeDestroy() {
-    if (this.animator) window.cancelAnimationFrame(this.animator)
-
-    // MUST delete the React view handle to prevent gigantic memory leak!
-    delete REACT_VIEW_HANDLES[this.viewId]
-  }
-
-  private startTime = Date.now()
-  private elapsed = 0
-  private animator: any = null
-
-  private animate() {
-    setTimeout(() => {
-      this.elapsed = (Date.now() - this.startTime) * 0.05
-      this.animator = window.requestAnimationFrame(this.animate)
-    }, 33)
-  }
-
-  private async loadBoundaries() {
-    if (!this.config.boundaries) return
-
-    try {
-      if (this.config.boundaries.startsWith('http')) {
-        const boundaries = await fetch(this.config.boundaries).then(async r => await r.json())
-        this.boundaries = boundaries.features
-      } else {
-        const boundaries = await this.fileApi.getFileJson(
-          `${this.subfolder}/${this.config.boundaries}`
-        )
-        this.boundaries = boundaries.features
-      }
-    } catch (e) {
-      console.error(e)
-      return
-    }
-    this.calculateCentroids()
-  }
-
-  private calculateCentroids() {
-    for (const feature of this.boundaries) {
-      const centroid: any = turf.centerOfMass(feature as any)
-      if (feature.properties[this.config.boundariesLabel]) {
-        centroid.properties.label = feature.properties[this.config.boundariesLabel]
-      }
-      centroid.properties.id = feature.properties[this.config.boundariesJoinCol]
-
-      this.centroids.push({
-        id: `${centroid.properties.id}`,
-        lon: centroid.geometry.coordinates[0],
-        lat: centroid.geometry.coordinates[1],
-      })
-    }
-    console.log({ centroids: this.centroids })
-    // for (const c of this.centroids) console.log(`${c.id},${c.lon},${c.lat}`)
-  }
-
-  private async loadDataset() {
-    try {
-      const dataset = await this.datamanager.getDataset(this.config)
-      // this.datamanager.addFilterListener(this.config, this.handleFilterChanged)
-
-      const data = dataset.allRows || ({} as any)
-
-      // assumes flow data has "origin,destination,count" columns
-      const origin = data.origin.values
-      const destination = data.destination.values
-      const count = data.count.values
-
-      const flows = [] as any[]
-      for (let i = 0; i < origin.length; i++) {
-        flows.push({
-          o: `${origin[i]}`,
-          d: `${destination[i]}`,
-          v: count[i],
-        })
-      }
-      this.flows = flows
-    } catch (e) {
-      const message = '' + e
-      console.log(message)
-      this.flows = []
-    }
-    console.log({ flows: this.flows })
-  }
-
-  private updateChart() {
-    // nothing, for now
   }
 }
 </script>
@@ -160,7 +38,7 @@ export default class VueComponent extends Vue {
 <style scoped lang="scss">
 @import '@/styles.scss';
 
-.map-layout {
+.deck-map {
   position: absolute;
   top: 0;
   bottom: 0;
@@ -168,10 +46,6 @@ export default class VueComponent extends Vue {
   right: 0;
   display: flex;
   flex-direction: column;
-}
-
-.map-layer {
-  flex: 1;
 }
 
 @media only screen and (max-width: 640px) {
