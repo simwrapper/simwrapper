8d926d51f4c478a444387888baa3990bb5458c68
Billy Charlton <charlton@vsp.tu-berlin.de>
2022-10-06 17:14:44 +0200

    WIP: animated,clustering flowmaplayer!

--- src/layers/FlowMapLayer.tsx
+++ src/layers/FlowMapLayer.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import DeckGL from '@deck.gl/react'
 import { StaticMap } from 'react-map-gl'
-import FlowMapLayer from '@flowmap.gl/core'
+import { FlowmapLayer } from '@flowmap.gl/layers'
 
 import { MAPBOX_TOKEN, REACT_VIEW_HANDLES } from '@/Globals'
 import globalStore from '@/store'
@@ -35,26 +35,47 @@ export default function Layer({
     globalStore.commit('setMapCamera', view)
   }
 
-  const layer = new FlowMapLayer({
-    id: 'my-flowmap-layer',
-    locations,
-    flows,
-    getFlowOriginId: flow => flow.o,
-    getFlowDestId: flow => flow.d,
-    getFlowMagnitude: flow => flow.v || null,
-    getLocationId: (location: any) => location.id,
-    getLocationCentroid: (location: any) => [location.lon, location.lat],
-    onHover: handleHover,
-    // animate: true,
-    // animationCurrentTime: elapsed,
-    // showTotals: true,
-    // selectedLocationIds: ['10'], //  '5', '10', '20'],
+  // const data = {
+  //   locations: [
+  //     { id: 'SF', lat: 37, lon: -122 },
+  //     { id: 'NYC', lat: 42, lon: -108 },
+  //     { id: 'Chicago', lat: 45, lon: -115 },
+  //   ],
+  //   flows: [
+  //     { o: 'SF', d: 'NYC', v: 100 },
+  //     { o: 'SF', d: 'Chicago', v: 50 },
+  //     { o: 'NYC', d: 'SF', v: 210 },
+  //     { o: 'NYC', d: 'Chicago', v: 30 },
+  //     { o: 'Chicago', d: 'NYC', v: 75 },
+  //     { o: 'Chicago', d: 'SF', v: 85 },
+  //   ],
+  // }
+  const layer = new FlowmapLayer({
+    data: props,
+    id: 'my-flowmap-layer' + viewId,
+    colorScheme: 'Teal',
+    adaptiveScalesEnabled: true,
+    animationEnabled: true,
+    clusteringEnabled: true,
+    clusteringAuto: true,
+    clusteringLevel: 20,
+    labelsEnabled: true,
+    locationLabelsEnabled: true,
+    darkMode: dark,
     pickable: true,
-    maxFlowThickness: 15,
-    maxLocationCircleSize: 20,
-    // opacity: 0.5,
+    // maxFlowThickness: 15,
+    // maxLocationCircleSize: 20,
+    opacity: 0.75,
     outlineThickness: -1,
-    showOnlyTopFlows: 2000,
+    showOnlyTopFlows: 20,
+    onHover: handleHover,
+    getLocationId: (location: any) => location.id,
+    getLocationLat: (location: any) => location.lat,
+    getLocationLon: (location: any) => location.lon,
+    getLocationName: (location: any) => location.id,
+    getFlowOriginId: (flow: any) => flow.o,
+    getFlowDestId: (flow: any) => flow.d,
+    getFlowMagnitude: (flow: any) => flow.v || null,
   })
 
   return (
@@ -64,7 +85,7 @@ export default function Layer({
       layers={[layer]}
       controller={true}
       viewState={viewState}
-      pickingRadius={5}
+      pickingRadius={4}
       getCursor={() => 'pointer'}
       onClick={handleClick}
       onViewStateChange={(e: any) => handleViewState(e.viewState)}
