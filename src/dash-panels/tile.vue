88d91c9fce8be4801ffb036e47f4e88acd6daf74
frievoe97 <friedrich.voelkers@me.com>
2023-05-19 15:34:03 +0200

    added font awesome support, added standard icons and started working on custom icon support

--- src/dash-panels/tile.vue
+++ src/dash-panels/tile.vue
@@ -1,19 +1,23 @@
 <template lang="pug">
   .content
     .tiles-container
-      .tile(v-for="(value, name, index) in this.dataSet.allRows" v-bind:style="{ 'background-color': colorsD3[index % colors.length]}")
+      .tile(v-if="imagesAreLoaded" v-for="(value, name, index) in this.dataSet.allRows" v-bind:style="{ 'background-color': colors[index % colors.length]}")
         p.tile-title {{ value.name }}
-        img.tile-image(v-if="value.values[1] != undefined" v-bind:src="'/src/assets/tile-icons/' + value.values[1] + '.png'")
         p.tile-value {{ value.values[0] }}
+        .tile-image(v-if="checkIfItIsACustomIcon(value.values[1])" :style="base64Images[index]") 
+        img.tile-image(v-else-if="checkIfIconIsInAssetsFolder(value.values[1])" v-bind:src="'/src/assets/tile-icons/' + value.values[1].trim() + '.svg'")
+        font-awesome-icon.tile-image(v-else :icon="value.values[1].trim()" size="2xl")
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import type { PropType } from 'vue'
 
+import readBlob from 'read-blob'
 import DashboardDataManager, { FilterDefinition } from '@/js/DashboardDataManager'
 import VuePlotly from '@/components/VuePlotly.vue'
 import { FileSystemConfig, Status, UI_FONT } from '@/Globals'
+import HTTPFileSystem from '@/js/HTTPFileSystem'
 import globalStore from '@/store'
 
 export default defineComponent({
@@ -63,25 +67,25 @@ export default defineComponent({
         },
       },
       colors: [
-        '#F08080', // Helles Korallenrosa
-        '#FFB6C1', // Blassrosa
-        '#FFDAB9', // Pfirsich
-        '#FFECB3', // Cremegelb
-        '#B0E0E6', // Hellblau
-        '#98FB98', // Hellgrün
-        '#FFD700', // Goldgelb
-        '#FFA07A', // Lachsrosa
-        '#E0FFFF', // Helltürkis
-        '#FFDAB9', // Rosa
-        '#FFC0CB', // Rosa
-        '#FFA500', // Orange
-        '#FF8C00', // Dunkelorange
-        '#FF7F50', // Korallenrot
-        '#FFE4B5', // Papaya
-        '#ADD8E6', // Hellblau
-        '#90EE90', // Hellgrün
-        '#FFD700', // Goldgelb
-        '#FFC0CB', // Rosa
+        '#F08080', // Light coral pink
+        '#FFB6C1', // Pale pink
+        '#FFDAB9', // peach
+        '#FFECB3', // cream yellow
+        '#B0E0E6', // light blue
+        '#98FB98', // light green
+        '#FFD700', // golden yellow
+        '#FFA07A', // salmon pink
+        '#E0FFFF', // light turquoise
+        '#FFDAB9', // pink
+        '#FFC0CB', // pink
+        '#FFA500', // orange
+        '#FF8C00', // dark orange
+        '#FF7F50', // coral red
+        '#FFE4B5', // papaya
+        '#ADD8E6', // light blue
+        '#90EE90', // light green
+        '#FFD700', // golden yellow
+        '#FFC0CB', // pink
         '#FFA500', // Orange
       ],
       colorsD3: [
@@ -96,18 +100,89 @@ export default defineComponent({
         '#BCBD22',
         '#17BECF',
       ],
+
+      localTileIcons: [
+        'departure_board',
+        'directions_car',
+        'emoji_transportation',
+        'local_taxi',
+        'subway',
+        'directions_bike',
+        'directions_subway',
+        'ev_station',
+        'local_gas_station',
+        'motorcycle',
+        'train',
+        'directions_boat',
+        'electric_car',
+        'group',
+        'local_parking',
+        'person',
+        'transportation',
+        'directions_bus',
+        'electric_rickshaw',
+        'groups',
+        'local_shipping',
+        'route',
+        'two_wheeler',
+      ],
+      testImage: '',
+      base64Images: [] as any[],
+      imagesAreLoaded: false,
     }
+  },
+  computed: {
+    fileApi(): HTTPFileSystem {
+      return new HTTPFileSystem(this.fileSystemConfig, globalStore)
+    },
   },
   async mounted() {
     this.dataSet = await this.loadData()
     this.validateDataSet()
     this.$emit('isLoaded')
+
+    Object.entries(this.dataSet.allRows).forEach(async (kv, i) => {
+      // const key = kv[0]
+      const value = kv[1] as any
+      // console.log(value)
+      if (this.checkIfItIsACustomIcon(value.values[1])) {
+        // console.log(this.subfolder + '/' + this.config.dataset)
+        try {
+          const blob = await this.fileApi.getFileBlob(
+            this.subfolder + '/' + this.config.dataset + '/../' + value.values[1]
+          )
+          const buffer = await readBlob.arraybuffer(blob)
+          const base64 = this.arrayBufferToBase64(buffer)
+          if (base64)
+            this.base64Images[i] = {
+              background: `center / cover no-repeat url(data:image/png;base64,${base64})`,
+            }
+        } catch (e) {
+          console.error(e)
+        }
+      }
+    })
+
+    // console.log(this.base64Images)
+    // setTimeout(() => (this.imagesAreLoaded = true), 100)
+    this.imagesAreLoaded = true
   },
   methods: {
+    arrayBufferToBase64(buffer: any) {
+      var binary = ''
+      var bytes = new Uint8Array(buffer)
+      var len = bytes.byteLength
+      for (var i = 0; i < len; i++) {
+        binary += String.fromCharCode(bytes[i])
+      }
+      return window.btoa(binary)
+    },
+
     async loadData() {
       try {
         this.validateYAML()
         let dataset = await this.datamanager.getDataset(this.config)
+        // console.log(dataset)
 
         // no filter? we are done
         if (!this.config.filters) return dataset
@@ -147,12 +222,30 @@ export default defineComponent({
         if (valueTemp.values.length > 1) {
           this.$store.commit('setStatus', {
             type: Status.WARNING,
-            msg: `The Dataset for the overview panel shoul have only two rows`,
+            msg: `The Dataset for the overview panel should have only two rows`,
             desc: `Check out the ${key}-column in the ${this.config.dataset} file.`,
           })
           break
         }
       }
+    },
+
+    checkIfIconIsInAssetsFolder(name: string) {
+      // console.log(name.trim(), this.localTileIcons.includes(name.trim()))
+      return this.localTileIcons.includes(name.trim())
+    },
+
+    checkIfItIsACustomIcon(name: string) {
+      if (
+        name.includes('.png') ||
+        name.includes('.jpg') ||
+        name.includes('.svg') ||
+        name.includes('.jpeg')
+      ) {
+        // console.log(this.fileSystemConfig.slug + '/' + this.subfolder + '/' + this.config.dataset)
+        return true
+      }
+      return false
     },
   },
 })
@@ -208,6 +301,7 @@ export default defineComponent({
 
 .tile .tile-value {
   font-size: 2rem;
+  font-weight: bold;
   width: 100%;
   color: #363636; // var(--text) but always the color from the light mode.
   grid-column-start: 2;
@@ -219,6 +313,8 @@ export default defineComponent({
 .tile .tile-title {
   width: 100%;
   font-size: 1.4rem;
+  height: 5rem;
+  margin-bottom: 0;
   color: #363636; // var(--text) but always the color from the light mode.
   text-align: center;
   grid-column-start: 1;
@@ -227,8 +323,9 @@ export default defineComponent({
 }
 
 .tile .tile-image {
-  height: 3.5rem;
+  height: 4rem;
   grid-row: 2;
+  align-items: baseline;
 }
 
 @media only screen and (max-width: 640px) {
