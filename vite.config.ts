458e66b58f5ef3dffe96143c90eca413674c1fd3
Billy Charlton <charlton@vsp.tu-berlin.de>
2023-09-22 13:37:00 +0200

    vite.config update: edit process config for vitest support

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,43 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue2'
 import markdownPlugin from 'unplugin-vue-markdown/vite'
 import pluginRewriteAll from 'vite-plugin-rewrite-all'
 
-export default defineConfig({
-  base: '/',
-  build: { sourcemap: true },
-  plugins: [
-    // vue
-    vue({ include: [/\.vue$/, /\.md$/] }),
-    //createVuePlugin(),
-    // markdown
-    markdownPlugin({}),
-    // pluginRewriteAll allows pages ending in http://path/blah.yaml to load
-    pluginRewriteAll(),
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
-  resolve: {
-    alias: {
-      '@': '/src',
-      '~': '/node_modules',
-      path: 'path-browserify',
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    base: '/',
+    build: { sourcemap: true },
+    plugins: [
+      // vue
+      vue({ include: [/\.vue$/, /\.md$/] }),
+      // markdown
+      markdownPlugin({}),
+      // pluginRewriteAll allows pages ending in http://path/blah.yaml to load
+      pluginRewriteAll(),
+    ],
+    test: {
+      globals: true,
+      environment: 'jsdom',
     },
-  },
-  define: {
-    // 'process.platform': null,
-    'process.env': [],
-  },
+    resolve: {
+      alias: {
+        '@': '/src',
+        '~': '/node_modules',
+        path: 'path-browserify',
+      },
+    },
+    define:
+      mode === 'test'
+        ? {
+            'process.env': [],
+          }
+        : {
+            'process.platform': null,
+            'process.env': [],
+          },
+  }
 })
