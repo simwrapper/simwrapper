7a4f26b140ff13aeb27c5f31b0d381c8615a20db
frievoe97 <friedrich.voelkers@me.com>
2023-11-29 18:03:04 +0100

    remove changes in TimeSlider

--- src/plugins/event-viewer/EventDeckMap.tsx
+++ src/plugins/event-viewer/EventDeckMap.tsx
@@ -63,7 +63,7 @@ export default function Component({
   projection = '',
 }) {
   // manage SimWrapper centralized viewState - for linked maps
-  const [viewState, setViewState] = useState(globalStore.state.viewState)
+  const [viewState, setViewState] = useState(INITIAL_VIEW)
 
   REACT_VIEW_HANDLES[viewId] = () => {
     setViewState(globalStore.state.viewState)
