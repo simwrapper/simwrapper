3107d57d1fb109f0f8706dacd7dbb0902e95cc79
Billy Charlton <charlton@vsp.tu-berlin.de>
2024-03-27 00:44:39 +0100

    wip: hdf5 viewer!

---
+++ src/plugins/matrix/H5Web.tsx
@@ -0,0 +1,25 @@
+import '@h5web/app/styles.css'
+
+import React, { useState } from 'react'
+import type { ChangeEvent } from 'react'
+import { App } from '@h5web/app'
+import { H5WasmProvider } from '@h5web/h5wasm'
+
+interface H5File {
+  filename: string
+  buffer: ArrayBuffer
+}
+
+function MyApp({ filename = '', buffer = undefined as any }) {
+  const h5File = { filename, buffer }
+
+  return (
+    <div style={{ height: '100%' }}>
+      <H5WasmProvider {...h5File}>
+        <App />
+      </H5WasmProvider>
+    </div>
+  )
+}
+
+export default MyApp
---
+++ src/plugins/matrix/MatrixViewer.vue
@@ -0,0 +1,168 @@
+<template lang="pug">
+.matrix-viewer
+  .flex-row
+    b-input(placeholder="filename.h5" v-model="filename")
+    b-button(@click="loadFile") Open
+
+  .thing(v-if="h5file")
+    H5Web.h5web(:filename="filename" :buffer="h5file")
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+import nprogress from 'nprogress'
+import YAML from 'yaml'
+
+import globalStore from '@/store'
+import { FileSystemConfig, VisualizationPlugin } from '@/Globals'
+import HTTPFileSystem from '@/js/HTTPFileSystem'
+
+import H5Web from './H5Web'
+
+const MyComponent = defineComponent({
+  name: 'MatrixViewer',
+  components: { H5Web },
+  props: {
+    root: { type: String, required: true },
+    subfolder: { type: String, required: true },
+    yamlConfig: String,
+    config: String,
+    thumbnail: Boolean,
+    cardId: String,
+  },
+  data: () => {
+    return {
+      globalState: globalStore.state,
+      filename: 'HWYALLAM.h5',
+      h5file: null as any,
+      subfolder: '',
+      yamlConfig: '',
+      thumbnail: false,
+      vizDetails: { title: '', description: '' } as any,
+      loadingText: 'Loading',
+      title: '',
+      description: '',
+    }
+  },
+  async mounted() {
+    this.thumbnail = this.thumbnail
+    this.yamlConfig = this.config || this.yamlConfig || '' // use whichever one was sent to us
+    this.subfolder = this.subfolder
+
+    // if (this.cardId) {
+    //   this.$emit('dimension-resizer', { id: this.cardId, resizer: this.changeDimensions })
+    // }
+
+    await this.getVizDetails()
+  },
+  computed: {
+    fileApi(): HTTPFileSystem {
+      return new HTTPFileSystem(this.fileSystem, globalStore)
+    },
+    fileSystem(): FileSystemConfig {
+      const svnProject: FileSystemConfig[] = this.$store.state.svnProjects.filter(
+        (a: FileSystemConfig) => a.slug === this.root
+      )
+      if (svnProject.length === 0) {
+        console.log('no such project')
+        throw Error
+      }
+      return svnProject[0]
+    },
+  },
+  watch: {
+    'globalState.isDarkMode'() {
+      // this.embedChart()
+    },
+    'globalState.resizeEvents'() {
+      // this.changeDimensions()
+    },
+
+    yamlConfig() {
+      this.yamlConfig = this.yamlConfig || ''
+      this.getVizDetails()
+    },
+
+    subfolder() {
+      this.subfolder = this.subfolder
+      this.getVizDetails()
+    },
+  },
+  methods: {
+    async loadFile() {
+      const filename = `/${this.filename}`
+      const response = await fetch(filename)
+      const buffer = await response.arrayBuffer()
+      this.h5file = buffer
+    },
+
+    // this happens if viz is the full page, not a thumbnail on a project page
+    buildRouteFromUrl() {
+      const params = this.$route.params
+      if (!params.project || !params.pathMatch) {
+        console.log('I CANT EVEN: NO PROJECT/PARHMATCH')
+        return
+      }
+
+      // subfolder and config file
+      const sep = 1 + params.pathMatch.lastIndexOf('/')
+      const subfolder = params.pathMatch.substring(0, sep)
+      const config = params.pathMatch.substring(sep)
+
+      this.subfolder = subfolder
+      this.yamlConfig = config
+    },
+
+    async getVizDetails() {
+      let yaml: any = {}
+
+      // might be a project config:
+      const filename = this.subfolder + '/' + this.yamlConfig
+      const text = await this.fileApi.getFileText(filename)
+      this.vizDetails = YAML.parse(text)
+
+      // this.loadingText = ''
+      // nprogress.done()
+    },
+  },
+})
+
+export default MyComponent
+</script>
+
+<style scoped lang="scss">
+@import '@/styles.scss';
+// @import '@h5web/app/dist/styles.css';
+
+.matrix-viewer {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  flex-direction: column-reverse;
+  padding: 1rem;
+  background-color: var(--bgDashboard);
+}
+
+.thing {
+  flex: 1;
+  background-color: lightyellow;
+  position: relative;
+}
+
+.h5web {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+
+.flex-row {
+  margin-top: 1rem;
+}
+</style>
