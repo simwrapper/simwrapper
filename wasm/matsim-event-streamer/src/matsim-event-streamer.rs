a1d43b5d93df4873240fe1e0720815b78de5b3d7
Billy Charlton <charlton@vsp.tu-berlin.de>
2024-12-03 15:49:14 +0100

    chunk partial results don't block

--- wasm/matsim-event-streamer/src/matsim-event-streamer.rs
+++ wasm/matsim-event-streamer/src/matsim-event-streamer.rs
@@ -1,7 +1,7 @@
 // use std::borrow::BorrowMut;
 // use std::io::{self, BufReader, Cursor, Empty, Read, Result, Seek, SeekFrom, Write};
 // use std::thread;
-// use zstd::Decoder;
+use zstd::Decoder;
 
 // use futures::prelude::*;
 // use futures::stream::{self, Stream, StreamExt};
@@ -22,9 +22,18 @@ impl ZstdDecompressor {
     }
 
     pub fn get_weather_stream(&mut self) -> Option<Vec<u8>> {
-        match self.rx.recv() {
+        match self.rx.try_recv() {
             Ok(data) => Some("hello".as_bytes().to_vec()), // Some(data),
-            Err(_) => None,
+            Err(std::sync::mpsc::TryRecvError::Empty) => {
+                // No data available right now
+                println!("No data ready");
+                None
+            }
+            Err(std::sync::mpsc::TryRecvError::Disconnected) => {
+                // Channel is closed
+                println!("Channel closed");
+                None
+            }
         }
     }
 }
@@ -39,24 +48,31 @@ fn main() {
     // read file
     let compressed = std::fs::read(&args[1]).expect("----Could not read file");
 
-    let (tx, rx) = mpsc::sync_channel(32768);
-
     // divide file into chunks to pass to dechunker
-    let chunk_size = 1000;
+    let chunk_size = 10000;
 
     let chunks = compressed.as_slice().chunks(chunk_size);
     println!("\n----File split into {} chunks", chunks.len());
 
+    // create comm channel and ZDecoder
+    let (tx, rx) = mpsc::sync_channel(32768);
     let mut zboot = ZstdDecompressor::new(rx);
 
     // let mut streamer = EventStreamer::new();
 
     for chunk in chunks {
         let mut data = Vec::new();
-        data.extend_from_slice(&chunk);
+        data.extend_from_slice(chunk);
         tx.send(data).expect("nope5");
-        // let json = streamer.process(data);
-        // println!("{json}")
+
+        match zboot.get_weather_stream() {
+            Some(partial_result) => {
+                println!("IN MAIN: {}", String::from_utf8_lossy(&partial_result));
+            }
+            None => {
+                println!("...not yet...");
+            }
+        }
     }
 
     // tx.send(String::from("String 1\n")).expect("nope1");
@@ -64,6 +80,6 @@ fn main() {
     drop(tx);
 
     while let Some(result) = zboot.get_weather_stream() {
-        println!("IN MAIN: {}", String::from_utf8_lossy(&result));
+        println!("FINAL: {}", String::from_utf8_lossy(&result));
     }
 }
