5ecaa1b52db955cffb58808e5464f808716c79dc
Billy Charlton <charlton@vsp.tu-berlin.de>
2025-02-14 14:41:42 +0100

    import "VIA" style link geometries if present

--- scripts/create-gmns-network.py
+++ scripts/create-gmns-network.py
@@ -7,19 +7,25 @@
 try:
     import sys, zipfile, csv, os
     import matsim
+    import pandas
+    from dfply import *
 except:
     print("OOPS! Error importing required libraries.")
-    print('try "pip3 install matsim-tools"')
+    print('try "pip3 install pandas dfply matsim-tools"')
 
-if len(sys.argv) != 3:
+if len(sys.argv) < 3:
     print(
-        "USAGE:  python create-gmns-network.py  [network]  [coord-system]"
+        "USAGE:  python create-gmns-network.py  [network]  [coord-system] (optional linkGeometries)"
     )
     sys.exit(1)
 
 p_network = sys.argv[1]
 p_coords = sys.argv[2]
+p_geom = len(sys.argv) > 3 and sys.argv[3] or None
+
 out_file = p_network + ".gmns.zip"
+
+if not p_geom: print("--- No link geometries specified. Stick network will be generated.")
 
 print("reading network:", p_network)
 network = matsim.read_network(p_network)
@@ -49,12 +55,34 @@ network.links = network.links.rename(columns={
     "to_node": "to_node_id",
 })
 
+network.links['geometry_id'] = network.links['link_id']
+
 # drop everything (for debug)
 # network.links = network.links[['link_id','from_node_id','to_node_id']]
 
 # write links
 network.links.to_csv("link.csv", index=False, quoting=csv.QUOTE_NONNUMERIC)
 os.system(trimcmd + "link.csv")
+
+# ----- geometry
+if p_geom:
+    print("Reading geometry")
+    geom = pandas.read_csv(p_geom)
+    geom['wkt'] = geom['Geometry'].apply(lambda g: (
+        'LINESTRING ' + ",".join([item.replace(',',' ') for item in g.split("),(")])
+    ))
+
+    del geom['Geometry']
+
+    geom = geom.rename(columns={
+    "LinkId": "geometry_id",
+    "wkt": "geometry",
+    })
+
+    # write geom
+    geom.to_csv("geometry.csv", index=False, quoting=csv.QUOTE_NONNUMERIC)
+    os.system(trimcmd + "geometry.csv")
+
 
 # write it out
 print("create zip:", out_file)
@@ -63,3 +91,5 @@ with zipfile.ZipFile(out_file, "w", compression=zipfile.ZIP_DEFLATED) as f:
     f.write("config.csv")
     f.write("node.csv")
     f.write("link.csv")
+    if p_geom: f.write("geometry.csv")
+
