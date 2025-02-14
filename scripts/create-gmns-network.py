21093f9982bb5124619399a6b62be6c9e70a8911
Billy Charlton <charlton@vsp.tu-berlin.de>
2025-02-14 13:18:53 +0100

    improve gmns script; quotes around IDs, pass CRS correctly

--- scripts/create-gmns-network.py
+++ scripts/create-gmns-network.py
@@ -1,12 +1,15 @@
-# matsim network converter
+#!/usr/bin/env python3
+
+# matsim network converter -> GMNS
+# this does NOT convert coordinates: They are kept in their original coordinate system
+# The GMNS npm library will convert this to proper 4326 (lng/lat) coordinates
+
 try:
-    import sys, json, zipfile
+    import sys, zipfile, csv, os
     import matsim
-    from dfply import *
-    from pyproj import Transformer
 except:
     print("OOPS! Error importing required libraries.")
-    print('try "pip3 install matsim-tools dfply pyproj"')
+    print('try "pip3 install matsim-tools"')
 
 if len(sys.argv) != 3:
     print(
@@ -14,12 +17,9 @@ if len(sys.argv) != 3:
     )
     sys.exit(1)
 
-
 p_network = sys.argv[1]
 p_coords = sys.argv[2]
 out_file = p_network + ".gmns.zip"
-
-coord_transformer = Transformer.from_crs(p_coords, "EPSG:4326")
 
 print("reading network:", p_network)
 network = matsim.read_network(p_network)
@@ -27,38 +27,34 @@ network = matsim.read_network(p_network)
 # ----- config
 # TODO: get real config from file instead of hardcoded everything
 config = """dataset_name,short_length,long_length,speed,crs,geometry_field_format,currency,version_number
-network,m,km,kph,25833,wkt,Euro cents,0.94"""
+network,m,km,kph,"""+p_coords+",wkt,Euro cents,0.94"
 
 with open("config.csv", "w") as f:
     f.write(config)
+
+# fix header line - pandas puts double quotes around column names, which Papaparse borks
+trimcmd = sys.platform == 'darwin' and """sed -i '' '1s/"//g' """ or """sed -i '1s/"//g' """
 
 # ----- nodes
 network.nodes = network.nodes.rename(columns={
     "x": "x_coord",
     "y": "y_coord",
 })
-network.nodes.to_csv("node.csv", index=False)
+network.nodes.to_csv("node.csv", index=False, quoting=csv.QUOTE_NONNUMERIC)
+os.system(trimcmd + "node.csv")
 
 # ----- links
 network.links = network.links.rename(columns={
     "from_node": "from_node_id",
     "to_node": "to_node_id",
 })
-network.links.to_csv("link.csv", index=False)
 
+# drop everything (for debug)
+# network.links = network.links[['link_id','from_node_id','to_node_id']]
 
-# # convert coords to lat/lon
-# print("reprojecting coordinates")
-# converted_links = {}
-# for link in links.values:
-#     coords = []
-
-#     fromY, fromX = coord_transformer.transform(link[1],link[2])
-#     coords.extend([round(fromX, 6), round(fromY, 6)])
-#     toY, toX = coord_transformer.transform(link[3], link[4])
-#     coords.extend([round(toX, 6), round(toY, 6)])
-
-#     converted_links[link[0]] = coords
+# write links
+network.links.to_csv("link.csv", index=False, quoting=csv.QUOTE_NONNUMERIC)
+os.system(trimcmd + "link.csv")
 
 # write it out
 print("create zip:", out_file)
