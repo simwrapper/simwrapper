095cb9bbe46c5be5d2bfda70f9160670229bbc1c
Billy Charlton <charlton@vsp.tu-berlin.de>
2025-03-03 19:51:10 +0100

    move to staging.simwrapper.app

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -44,7 +44,6 @@ declare module 'zip-loader'
 declare module 'react-toggle'
 declare module 'vue-good-table'
 declare module 'vue-virtual-scroll-list'
-
 declare module '@turf/point-to-line-distance'
 declare module '@turf/nearest-point-to-line'
 declare module '@loaders.gl/images'
