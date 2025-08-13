9b95f6e418bcc801007faef9113d6da6b2743630
Billy Charlton <mail@billyc.cc>
2025-08-13 11:50:55 +0200

    test1

--- tests/e2e/aggregate-od.spec.ts
+++ tests/e2e/aggregate-od.spec.ts
@@ -1,7 +1,19 @@
 import { test, expect } from '@playwright/test'
 
-test('aggregate-od loads berlin data', async ({ page }) => {
+import fs from 'fs'
+
+test('aggregate-od loads berlin data', async ({ page, browserName }) => {
+  // console.log
+  const logStream = fs.createWriteStream(`test-results/console-${browserName}.log`, { flags: 'a' })
+  page.on('console', msg => {
+    const message = `[${msg.type()}] ${msg.text()}\n`
+    console.log(message.trim())
+    logStream.write(message)
+  })
+
   await page.goto('/e2e-tests/agg-od')
+  await page.waitForTimeout(5000)
+  await page.screenshot({ path: `test-results/screenshot1-${browserName}.png`, fullPage: true })
 
   // wait for testdata to be populated
   await page.waitForFunction(() => {
@@ -15,6 +27,7 @@ test('aggregate-od loads berlin data', async ({ page }) => {
     //   testdata.geojson?.length
     // )
   })
+  await page.screenshot({ path: `test-results/screenshot2-${browserName}.png`, fullPage: true })
 
   // check testdata
   //@ts-ignore
