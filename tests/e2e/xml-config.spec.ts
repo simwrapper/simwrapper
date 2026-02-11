94fbe774ddfbc664ce864bd650492bd867f3590f
frievoe97 <friedrich.voelkers@me.com>
2026-02-11 08:46:54 +0100

    remove time, opacity, and metric value from tooltip

--- tests/e2e/xml-config.spec.ts
+++ tests/e2e/xml-config.spec.ts
@@ -1,11 +1,10 @@
 import { test, expect } from '@playwright/test'
 
 test('load cottbus XML Config', async ({ page }) => {
-  test.setTimeout(120_000)
   await page.goto('e2e-tests/cottbus/output_config.xml.gz')
 
   const leaves = page.locator('.leaf-node') as any
-  await page.waitForSelector('.leaf-node', { timeout: 120_000 })
-  await expect(leaves).toHaveCount(25, { timeout: 120_000 })
+  await expect(leaves.nth(0)).toBeVisible()
+  await expect(leaves).toHaveCount(25)
   await expect(leaves.nth(0)).toHaveText('config')
 })
